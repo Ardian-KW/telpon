d3d695e50b4fb634813823d32e7ef340003ccd3a
Ardian-KW <54944329+Ardian-KW@users.noreply.github.com>
2019-09-06 08:58:36 +0700

    Create telpon.sh

---
+++ telpon.sh
@@ -0,0 +1,20 @@
+#!/system/xbin/bash
+clear
+figlet "TELPON"
+sleep 1
+echo "Fake TELPON Tokopedia"
+sleep 1
+echo "Author: Ardian Kw"
+sleep 1
+echo "contact: kambingtua700@gmail.com"
+sleep 1
+echo " When Your Broke My Heart We Can Broke Your Security"
+sleep 1
+echo "Ketik ok Untuk Lanjut"
+read mrrm
+if [ $mrrm = ok ] || [ $mrrm = ok ]
+then
+clear
+figlet "TELPON"
+php ok.php
+fi
