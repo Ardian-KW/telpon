7adbb1c1b7297e74a706603542be0ac287bdba84
Ardian-KW <54944329+Ardian-KW@users.noreply.github.com>
2019-09-06 10:08:50 +0700

    Update telp.sh

--- telp.sh
+++ telp.sh
@@ -1,9 +1,11 @@
 #!/system/xbin/bash
 clear
-echo "\033[36;1m"
-figlet "Fake Call"
+echo "\033[32;1m"
+figlet "Ardian Kw"
 sleep 1
-echo "\033[31;1m Fake Call Tokopedia"
+echo "\033[36;1m"
+fighlet "Telp Palsu"
+echo "\033[31;1m Telp Palsu Tokopedia"
 sleep 1
 echo "\033[33;1m Author: Ardian Kw"
 sleep 1
@@ -17,6 +19,6 @@ if [ $mrrm = 1 ] || [ $mrrm = 1 ]
 then
 clear
 echo "\033[32;1m"
-figlet "Ardian Kw"
+figlet "Telp Palsu"
 php 1.php
 fi
